d8645de19bc9b930b66434e1fdf20bd111918f43
Štěpán Slavík <sslavik@Stepan-MacBook-Pro.local>
2024-04-23 20:17:34 +0200

    Set your own greeting and name.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut arguments = env::args();
+    let _ = arguments.next().unwrap();
+    let greeting = arguments.next().unwrap_or("Hello".to_string());
+
+    println!("Enter your name:");
+    let mut name = String::new();
+    io::stdin().read_line(&mut name).expect("Read line failed!");
+    let name = name.trim();
+
+    println! {"{greeting} {name}!"}
 }
